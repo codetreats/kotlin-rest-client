6449b804096edaad6c4dcf259eebfe7375e73cdc
martin <github@msturm.net>
2026-02-22 12:02:10 +0100

    add response headers to response

--- src/main/kotlin/net/codetreats/rest/RestClient.kt
+++ src/main/kotlin/net/codetreats/rest/RestClient.kt
@@ -19,7 +19,7 @@ import java.nio.file.Files
  * @param code the status code
  * @param message the message body if available
  */
-data class Response(val code: Int, val message: String)
+data class Response(val code: Int, val message: String, val headers: Map<String, List<String>> = emptyMap())
 
 /**
  * A status code range
@@ -113,8 +113,9 @@ class RestClient(
             }
             val statusCode = answer.status.value
             val text = answer.bodyAsText()
+            val responseHeaders = answer.headers.entries().associate { it.key to it.value }
             if (allowedStatusCodes.fromInclusive <= statusCode && statusCode < allowedStatusCodes.toExclusive) {
-                return@runBlocking Response(statusCode, text)
+                return@runBlocking Response(statusCode, text, responseHeaders)
             } else {
                 throw IllegalStateException("Received unexpected status code: $statusCode, $text")
             }
@@ -151,8 +152,9 @@ class RestClient(
             }
             val statusCode = answer.status.value
             val text = answer.bodyAsText()
+            val responseHeaders = answer.headers.entries().associate { it.key to it.value }
             if (allowedStatusCodes.fromInclusive <= statusCode && statusCode < allowedStatusCodes.toExclusive) {
-                return@runBlocking Response(statusCode, text)
+                return@runBlocking Response(statusCode, text, responseHeaders)
             } else {
                 throw IllegalStateException("Received unexpected status code: $statusCode, $text")
             }
